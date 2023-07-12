e800f8ae44a005eb0b3c78bae60c59237a0d0b2e
Mouleaswar <74062509+Mouleas@users.noreply.github.com>
2023-07-12 17:34:51 +0530

    Update ApiConstants.js

--- src/constants/ApiConstants.js
+++ src/constants/ApiConstants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "https://localhost:7213/api";
+export const BASE_URL = "https://bookwormapp.azurewebsites.net/api";
 export const QUOTES_BASE_URL = "https://type.fit/api/quotes";
-export const BOOK_IMAGE_URL = "https://localhost:7213/uploads/books/";
+export const BOOK_IMAGE_URL = "https://bookwormapp.azurewebsites.net/uploads/books/";
 
 // https://bookwormapp.azurewebsites.net
